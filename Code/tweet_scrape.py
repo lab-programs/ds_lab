d2bde7350b1806d49464697609f4f193e36f5e62
Ranganath395 <55680190+Ranganath395@users.noreply.github.com>
2021-10-27 23:18:45 +0530

    Update tweet_scrape.py

--- Code/tweet_scrape.py
+++ Code/tweet_scrape.py
@@ -5,7 +5,7 @@ import json
 # Creating list to append tweet data to
 tweets_list2 = {}
 
-# Using TwitterSearchScraper to scrape data and append tweets to list
+# Using TwitterSearchScraper to scrape data and append tweets to list.
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper('#mannkibaat since:2021-09-26 until:2021-09-27').get_items()):
     if i>1000:
         break
