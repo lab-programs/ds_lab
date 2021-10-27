c96aeff339d472168625f593c9addcb11f79d138
Yadu9238 <krishnanyadu60@gmail.com>
2021-10-27 06:26:23 +0530

    Code update

---
+++ Code/extract_date.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup, SoupStrainer
+from tqdm import tqdm
+import requests
+import re
+import os.path 
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+my_dates = []
+txt_file = open("Date.txt", "w")
+my_links = open('output.txt').readlines()
+for url in tqdm(my_links[1:]):
+	driver = webdriver.Firefox(options = options)
+	driver.get(url)
+
+	driver.implicitly_wait(1)
+	content = driver.page_source
+	date_idx = content.find('<span class=\"date\"')
+	date = content[date_idx+19:date_idx + 31]
+	txt_file.write(date+"\n")
+	#my_dates.append(date)
+'''
+with open("Date.txt", "w") as txt_file:
+	for line in my_links:
+		txt_file.write("".join(line) + "\n")
+'''
+
+#print(content)
---
+++ Code/tweet_scrape.py
@@ -0,0 +1,17 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import json
+
+# Creating list to append tweet data to
+tweets_list2 = {}
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('#mannkibaat since:2021-09-26 until:2021-09-27').get_items()):
+    if i>1000:
+        break
+    tweets_list2[tweet.id]  = tweet.content
+    
+# Creating a dataframe from the tweets list above
+f = open("tweets.json","w+")
+json.dump(tweets_list2,f,ensure_ascii = True)
+
